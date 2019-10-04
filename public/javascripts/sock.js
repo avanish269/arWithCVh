45a5888bbb4219641879aed75822fa45d5cb6a46
avanish269 <avanish269@gmail.com>
2019-10-04 06:08:47 +0530

    maybe

--- public/javascripts/sock.js
+++ public/javascripts/sock.js
@@ -1,5 +1,32 @@
-var socket=io.connect('http://localhost:8000');
-socket.on('image', (imag)=>{
-  var image=document.getElementById('image');
-  image.src=`data:image/jpeg;base64,${imag}`;
-})
+var constraints={video: {facingMode: "environment"}, audio: false};
+var cameraView=document.querySelector("#camera--view");
+var cameraOutput=document.querySelector("#camera--output");
+var cameraSensor=document.querySelector("#camera--sensor");
+var ctx=cameraSensor.getContext("2d");
+
+cameraSensor.style.display="none";
+cameraView.style.display="none";
+
+function cameraStart(){
+  navigator.mediaDevices.getUserMedia(constraints).then(function(stream){
+    track=stream.getTracks()[0];
+    cameraView.srcObject=stream;
+  }).catch(function(error){
+    console.error("Oops. Something is broken.", error);
+  });
+}
+
+setInterval(cameraT, 1000);
+function cameraT(){
+  cameraSensor.width=cameraView.videoWidth;
+  cameraSensor.height=cameraView.videoHeight;
+  ctx.translate(cameraSensor.width, 0);
+  ctx.scale(-1, 1);
+  ctx.drawImage(cameraView, 0, 0);
+  ctx.translate(cameraSensor.width, 0);
+  ctx.scale(-1, 1);
+  cameraOutput.src=cameraSensor.toDataURL();
+  console.log(cameraOutput.src);
+}
+
+window.addEventListener("load", cameraStart, false);
--- start.js
+++ start.js
@@ -1,17 +1,6 @@
 var app=require('./app');
 var server=require('http').Server(app);
 var io=require('socket.io')(server);
-var cv=require('opencv4nodejs');
-var wCap=new cv.VideoCapture(0);
-
-var FPS=10;
-
-setInterval(function sio(){
-  var frame=wCap.read();
-  var image=cv.imencode('.jpg', frame).toString('base64');
-  io.emit('image', image);
-
-}, 1000/FPS)
 
 var ser=server.listen(8000, serverFunc);
 
