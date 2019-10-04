7dea77ac7c3c971f5d7772542cbc214d2a80871d
avanish269 <avanish269@gmail.com>
2019-10-04 13:18:51 +0530

    hey

--- start.js
+++ start.js
@@ -2,7 +2,7 @@ var app=require('./app');
 var server=require('http').Server(app);
 var io=require('socket.io')(server);
 
-var ser=server.listen(8000, serverFunc);
+var ser=server.listen(process.env.PORT || 8000, serverFunc);
 
 function serverFunc(){
   console.log(`Express is running on port ${server.address().port}`);
