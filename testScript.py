1d79950400ed4295a80f35fc218abcfe7fe7a411
TANMAY912 <tanudim2012@gmail.com>
2019-12-08 22:51:15 +0530

    Update testScript.py

--- testScript.py
+++ testScript.py
@@ -4,6 +4,7 @@ import base64
 from PIL import Image
 import numpy as np
 from io import BytesIO
+import random
 
 data=""
 
@@ -15,12 +16,18 @@ b=base64.b64decode(data)
 bio=BytesIO(b)
 im=Image.open(bio).convert('RGB')
 np_im=np.array(im)
-result=str(np_im.shape[0])
-result+=", "
-result+=str(np_im.shape[1])
-result+=", "
-result+=str(np_im.shape[2])
-result+=", "
-result+=str(np_im[0, 0, 0])
+# result=str(np_im.shape[0])
+# result+=", "
+# result+=str(np_im.shape[1])
+# result+=", "
+# result+=str(np_im.shape[2])
+# result+=", "
+# result+=str(np_im[0, 0, 0])
+
+#Insert The code and return cordinate
+
+res = random.randint(0, 9)
+result = str(res)
+
 sys.stdout.write(result)
 sys.stdout.flush()
