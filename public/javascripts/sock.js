32ec57a246c59d9c0b9740690fdc81b21ecfec16
TANMAY912 <tanudim2012@gmail.com>
2019-12-08 22:49:17 +0530

    Update sock.js

--- public/javascripts/sock.js
+++ public/javascripts/sock.js
@@ -2,10 +2,20 @@ var constraints={video: {facingMode: "environment"}, audio: false};
 var cameraView=document.querySelector("#camera--view");
 var cameraOutput=document.querySelector("#camera--output");
 var cameraSensor=document.querySelector("#camera--sensor");
+var arobj = document.querySelector("#clippedDiv");
 var ctx=cameraSensor.getContext("2d");
 var blobURL="";
+var w = screen.width;
+var h = screen.height;
+w = document.documentElement.clientWidth;
+h = document.documentElement.clientHeight;
 cameraSensor.style.display="none";
-cameraView.style.display="none";
+cameraView.width=w;
+cameraView.height=h;
+arobj.style.width=w;
+arobj.style.height=h;
+// cameraView.style.display="none";
+cameraOutput.style.display="none";
 
 function cameraStart(){
   navigator.mediaDevices.getUserMedia(constraints).then(function(stream){
@@ -20,6 +30,14 @@ setInterval(cameraT, 1000);
 function cameraT(){
   cameraSensor.width=cameraView.videoWidth;
   cameraSensor.height=cameraView.videoHeight;
+  // cameraSensor.width=window.innerWidth;
+  // cameraSensor.height=window.innerHeight;
+  // console.log(window.innerHeight);
+  // cameraView.width=window.innerWidth;
+  // cameraView.height=window.innerHeight;
+  // arobj.style.width=window.innerWidth;
+  // arobj.style.height=window.innerHeight;
+  // arobj.style.display = "none";
   ctx.translate(cameraSensor.width, 0);
   ctx.scale(-1, 1);
   ctx.drawImage(cameraView, 0, 0);
@@ -30,8 +48,22 @@ function cameraT(){
   }, 'image/png');
   cameraOutput.src=cameraSensor.toDataURL();
   var socket=io.connect(location.origin);
-  console.log(cameraOutput.src);
+  // console.log(cameraOutput.src);
   socket.emit('Image Data', {data1: blobURL, data2: cameraOutput.src});
+  console.log('sent')
+  socket.on('message', function(data){
+    // cord_t = data;
+    // console.log(data)
+    fin = parseInt(data) % 2
+    // if (fin == 0) {
+    //   arobj.style.display = "none";
+    // } else {
+    //
+    // }
+    console.log(fin)
+  });
 }
+
+
 
 window.addEventListener("load", cameraStart, false);
